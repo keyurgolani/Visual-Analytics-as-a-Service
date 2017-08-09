1147ace26c44f0f8b9d67452f2c71f1a21134665
Ikwhan Chang <matthew.chang@urhy.me>
2017-08-08 22:17:44 -0700

    Add Developments folder, make the default structure of GUI application by using Yeoman - generator-angular

---
+++ Developments/GUI/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name visualAnalyticsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the visualAnalyticsApp
+ */
+angular.module('visualAnalyticsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
