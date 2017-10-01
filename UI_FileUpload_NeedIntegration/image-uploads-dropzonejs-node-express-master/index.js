54c6573f8b9364536c06bba472eeb81a47137670
Ashwini <ashwinicsanjose@gmail.com>
2017-10-01 16:25:41 -0700

    New files needed for file upload and download

---
+++ UI_FileUpload_NeedIntegration/image-uploads-dropzonejs-node-express-master/index.js
@@ -0,0 +1,26 @@
+var express   =   require( 'express' );
+var multer    =   require( 'multer' );
+var upload    =   multer( { dest: 'uploads/' } );
+var sizeOf    =   require( 'image-size' );
+var exphbs    =   require( 'express-handlebars' );
+require( 'string.prototype.startswith' );
+
+var app = express();
+
+app.use( express.static( __dirname + '/bower_components' ) );
+
+app.engine( '.hbs', exphbs( { extname: '.hbs' } ) );
+app.set('view engine', '.hbs');
+
+app.get( '/', function( req, res, next ){
+  return res.render( 'index' );
+});
+
+app.post( '/upload', upload.single( 'file' ), function( req, res, next ) {
+
+  return res.status( 200 ).send( req.file );
+});
+
+app.listen( 8080, function() {
+  console.log( 'Express server listening on port 8080' );
+});
