4c8ac7a6db5d134524c5614573c4ad61777e99f7
Narasa Kumar <knarasa@gmail.com>
2017-12-04 20:51:51 -0800

    database updated sql - script column in USERPROFILE table and varchar data limit for userjobhistory table

--- Database/VAAAS_db_SETUP_NEW.sql
+++ Database/VAAAS_db_SETUP_NEW.sql
@@ -21,11 +21,13 @@ CREATE TABLE `USERPROFILE` (
   `user_id` bigint(20) NOT NULL,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
+  `script` varchar(10000) DEFAULT NULL,
   PRIMARY KEY (`profile_id`),
   UNIQUE KEY `profile_id_UNIQUE` (`profile_id`),
   KEY `fk_USERPROFILE_1_idx` (`user_id`),
   CONSTRAINT `fk_USERPROFILE_1` FOREIGN KEY (`user_id`) REFERENCES `USERACCOUNT` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
 
 CREATE TABLE `USERJOBHISTORY` (
   `job_id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -99,16 +101,17 @@ CREATE TABLE `PUBLICDATA` (
 
 
 CREATE TABLE `DATASETS` (
-  `dataset_id` BIGINT(10) NOT NULL AUTO_INCREMENT,
-  `beautiful_name` VARCHAR(45) NULL,
-  `filename` VARCHAR(45) NOT NULL,
-  `root_path` VARCHAR(45) NOT NULL,
-  `owner` BIGINT(10) ZEROFILL NULL,
-  `is_private` TINYINT NOT NULL,
+  `dataset_id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `beautiful_name` varchar(1024) DEFAULT NULL,
+  `filename` varchar(256) NOT NULL,
+  `root_path` varchar(1024) NOT NULL,
+  `owner` bigint(10) unsigned zerofill DEFAULT NULL,
+  `is_private` tinyint(4) NOT NULL,
   PRIMARY KEY (`dataset_id`),
-  UNIQUE INDEX `dataset_id_UNIQUE` (`dataset_id` ASC),
-  UNIQUE INDEX `beautiful_name_UNIQUE` (`beautiful_name` ASC),
-  UNIQUE INDEX `unique_path` (`filename` ASC, `root_path` ASC));
+  UNIQUE KEY `dataset_id_UNIQUE` (`dataset_id`),
+  UNIQUE KEY `unique_path` (`filename`,`root_path`),
+  UNIQUE KEY `beautiful_name_UNIQUE` (`beautiful_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 
 COMMIT;
